50216e6a3279bf78b0ab0c9c28e787b788092f4b
NellyMagaramova <jasmin_oir@mail.ru>
2025-05-02 15:36:24 +0300

    facade

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -2,12 +2,10 @@ package org.test;
 
 import org.test.facade.CarFacade;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
           CarFacade  carFacade = new CarFacade();
           carFacade.go();
     }
